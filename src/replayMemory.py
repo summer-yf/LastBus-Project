7f8c5f5f6599990584d268c2ed522e69f9de714c
jeffrey_laptop <z5259101@ad.unsw.edu.au>
2022-07-24 15:55:34 +1000

    Added replay memory class

---
+++ src/replayMemory.py
@@ -0,0 +1,19 @@
+from random import sample
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, transition):
+        if len(self.memory == capacity)
+            del self.memory[0]
+        self.memory.append(transition)
+        return
+
+    def sample(self, batch_size):
+        sample(self.memory, batch_size)
+        return
+        
+    def __len__(self):
+        return len(self.memory)
