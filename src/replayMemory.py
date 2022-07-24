904215305904550b578de9a6e133dcdda3c482f8
jeffrey_laptop <z5259101@ad.unsw.edu.au>
2022-07-24 18:35:34 +1000

    fixed typo

--- src/replayMemory.py
+++ src/replayMemory.py
@@ -6,13 +6,13 @@ class ReplayMemory:
         self.memory = []
 
     def push(self, transition):
-        if len(self.memory == self.capacity):
+        if len(self.memory) == self.capacity:
             del self.memory[0]
         self.memory.append(transition)
         return
 
     def sample(self, batch_size):
-        sample(self.memory, batch_size)
+        sample(self.memory, min(len(self.memory), batch_size))
         return
 
     def __len__(self):
